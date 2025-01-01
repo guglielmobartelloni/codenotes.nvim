2a329194ec1f1d658bcf410a9dbb01b71b3f4c78
guglielmobartelloni <bartelloni.guglielmo@gmail.com>
2025-01-01 16:13:19 +0100

    feat: add resume of the opened split window if already is opened

--- lua/neatnotes/core.lua
+++ lua/neatnotes/core.lua
@@ -16,13 +16,19 @@ function M.get_selected_lines()
 end
 
 function M.open_split_window(file_path)
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		local name = vim.api.nvim_buf_get_name(buf)
+		if name == file_path then
+			vim.api.nvim_set_current_win(win)
+			return buf
+		end
+	end
+
 	vim.cmd("vsplit")
 	local win = vim.api.nvim_get_current_win()
-	local buf = vim.api.nvim_create_buf(true, false)
-	vim.api.nvim_win_set_buf(win, buf)
 	vim.cmd("edit " .. vim.fn.fnameescape(file_path))
-	buf = vim.api.nvim_get_current_buf()
-	return buf
+	return vim.api.nvim_win_get_buf(win)
 end
 
 function M.open_file_in_floating_window(file_path)
@@ -111,12 +117,12 @@ function M.append_to_markdown(lines, file_path, start_line, end_line)
 	-- Move the cursor to the end of the `- [%s:%d-%d]` line
 	vim.api.nvim_win_set_cursor(0, { vim.api.nvim_buf_line_count(note_buf), #ref_lines[#ref_lines] })
 
-	-- Add the code block
-	vim.api.nvim_buf_set_lines(note_buf, -1, -1, false, { "```" .. current_buf_lang })
+	-- Add the code block with spaces indentation
+	vim.api.nvim_buf_set_lines(note_buf, -1, -1, false, { "    " .. "```" .. current_buf_lang })
 	for _, line in ipairs(lines) do
-		vim.api.nvim_buf_set_lines(note_buf, -1, -1, false, { "    " .. line })
+		vim.api.nvim_buf_set_lines(note_buf, -1, -1, false, { "    " .. "    " .. line })
 	end
-	vim.api.nvim_buf_set_lines(note_buf, -1, -1, false, { "```" })
+	vim.api.nvim_buf_set_lines(note_buf, -1, -1, false, { "    " .. "```" })
 end
 
 -- Main function to handle the workflow
